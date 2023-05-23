314120a52bc5d4987ff5106cae1a30d5e6ab785b
Marcos Bernasconi <xb3rna@gmail.com>
2023-05-23 20:25:04 +0200

    Current API endpoint is working

---
+++ src/pages/api/movies.ts
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// type Data = {
+//   name: string
+// }
+
+const myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer Wookie2021");
+myHeaders.append("Content-Type", "application/json");
+
+export default function handler(
+  req: NextApiRequest,
+  res: any // res: NextApiResponse<Data>
+) {
+  fetch("https://wookie.codesubmit.io/movies", {
+    method: "GET",
+    headers: myHeaders,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => res.status(200).json(response))
+    .catch((error) => {
+      console.error("Error:", error);
+      res.status(500).send("Internal Server error");
+    });
+}
