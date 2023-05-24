928275542ad4795ede77fb4a4906c4a39063b318
Marcos Bernasconi <xb3rna@gmail.com>
2023-05-24 17:09:03 +0200

    Created getServerSideProp to make dynamic Routing and to fetch the data of the movie, display also the data in the component

--- src/pages/movie/[slug].tsx
+++ src/pages/movie/[slug].tsx
@@ -1,18 +1,26 @@
 import { useState, useEffect, useRef } from "react";
 import { Navbar } from "@/Components/Navbar";
-import { Footer } from "../Components/Footer";
-export default function Home() {
-  const [moviesInformation, setMoviesInformation] = useState();
+import { Footer } from "../../Components/Footer";
+import { useRouter } from "next/router";
 
-  useEffect(() => {
-    fetch("http://localhost:3000/api/movies")
-      .then((response) => {
-        return response.json();
-      })
-      .then((movies) => {
-        setMoviesInformation(movies);
-      });
-  }, []);
+export async function getServerSideProps(context: any) {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer Wookie2021");
+  myHeaders.append("Content-Type", "application/json");
+  const { slug } = context.params;
+  const result = await fetch(`https://wookie.codesubmit.io/movies/${slug}`, {
+    method: "GET",
+    headers: myHeaders,
+  });
+  const movie = await result.json();
+
+  return { props: movie };
+}
+
+export default function Home(movie: any) {
+  const [movieInformation, setMovieInformation] = useState(movie);
+  const date = movie.released_on.split("-")[0];
+  console.log(movie);
 
   return (
     <>
@@ -20,51 +28,51 @@ export default function Home() {
 
       <main className=" w-screen  bg-neutral-200 ">
         <section className="mx-auto max-w-[100rem] flex-row items-center pt-8 lg:flex ">
-          <div className="mx-auto h-[20rem] w-[14rem] rounded-xl border-2 border-neutral-200 bg-white  lg:mx-[10%]"></div>
+          <div className="mx-auto h-[20rem] w-[14rem] overflow-hidden rounded-xl border-2 border-neutral-200 bg-white  lg:mx-[10%]">
+            <img
+              className="h-full w-full object-cover"
+              src={movie.poster}
+              alt="poster"
+            />
+          </div>
           <div className="flex-grow bg-white lg:rounded-tl-lg">
             <div className="mx-auto mt-6 max-w-[40rem] bg-white px-8 pb-8 pt-4  lg:mx-[2rem]">
               <div className="flex flex-row items-baseline gap-4 ">
-                <p className="text-3xl font-semibold">
-                  Spider-Man: Into the Spider-Verse
-                </p>
+                <p className="text-3xl font-semibold">{movie.title}</p>
                 <p className="rounded-lg border-[1px] bg-neutral-50 px-1 py-2 font-semibold text-neutral-500">
-                  9/10
+                  {movie.imdb_rating}+/10
                 </p>
               </div>
               <div>
                 <div className="mt-2 flex flex-row divide-x-2 divide-neutral-200">
-                  <p className="px-2 text-neutral-500">2018</p>
-                  <p className="px-2 text-neutral-500">2h 29min</p>
+                  <p className="px-2 text-neutral-500">{date}</p>
+                  <p className="px-2 text-neutral-500">{movie.length}</p>
                   <p className="px-2 text-neutral-500">★★★★★</p>
                 </div>
                 <ul className="mt-4 flex flex-row gap-3">
                   Directors:
-                  <li className="underline underline-offset-1 ">
-                    Anthony Russo
-                  </li>
-                  <li className="underline underline-offset-1 ">Joe Russo</li>
+                  {movie.director.map((directorName: string) => {
+                    return (
+                      <li className="underline underline-offset-1 ">
+                        {directorName}
+                      </li>
+                    );
+                  })}
                 </ul>
                 <div className="mt-4 flex flex-row items-center gap-3">
                   Cast:
                   <ul className=" flex flex-row items-start gap-2 overflow-auto">
-                    <li className="whitespace-nowrap rounded-md border-[1px] border-neutral-200 bg-neutral-50 px-2 py-1 italic text-neutral-600">
-                      Shameik Moore
-                    </li>
-                    <li className="whitespace-nowrap rounded-md border-[1px] border-neutral-200 bg-neutral-50 px-2 py-1 italic text-neutral-600">
-                      Jake Johnson
-                    </li>
-                    <li className="whitespace-nowrap rounded-md border-[1px] border-neutral-200 bg-neutral-50 px-2 py-1 italic text-neutral-600">
-                      Hailee Steinfeld
-                    </li>
+                    {movie.cast.map((castName: string) => {
+                      return (
+                        <li className="whitespace-nowrap rounded-md border-[1px] border-neutral-200 bg-neutral-50 px-2 py-1 italic text-neutral-600">
+                          {castName}
+                        </li>
+                      );
+                    })}
                   </ul>
                 </div>
               </div>
-              <p className="mt-6 text-neutral-500">
-                Miles Morales is juggling his life between being a high school
-                student and being a spider-man. When Wilson \"Kingpin\" Fisk
-                uses a super collider, others from across the Spider-Verse are
-                transported to this dimension.
-              </p>
+              <p className="mt-6 text-neutral-500">{movie.overview}</p>
               <div className="flex flex-row gap-6">
                 <button className="mt-12 flex flex-row items-center gap-3 rounded-lg bg-black px-4 py-2 text-xl font-semibold text-white">
                   <svg
