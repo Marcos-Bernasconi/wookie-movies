b873b293c7a65ce62479454deea439a3ec7cff86
Marcos Bernasconi <xb3rna@gmail.com>
2023-05-24 15:27:18 +0200

    Responsive layout of the movie page is complete

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,6 +3,7 @@ export function Footer() {
     <footer className="mt-8 w-full bg-white px-8 pb-16 pt-8">
       <h3 className="text-lg font-semibold text-black">Wookie Movies</h3>
       <p className="text-sm text-neutral-500">Copyright © 2023 Wookie Movies</p>
+      <p className="text-xs italic text-neutral-400">By Marcos Bernasconi</p>
     </footer>
   );
 }
