498d8209f39265872357c4a44f586d62ae071d1e
Marcos Bernasconi <xb3rna@gmail.com>
2023-05-25 09:16:49 +0200

    Completed and ready to be delivered

--- src/pages/movie/[slug].tsx
+++ src/pages/movie/[slug].tsx
@@ -2,7 +2,8 @@ import { useState, useEffect, useRef } from "react";
 import { Navbar } from "@/Components/Navbar";
 import { Footer } from "../../Components/Footer";
 import { MovieDescription } from "../../Components/MovieDescription";
-import { useRouter } from "next/router";
+
+import { movie } from "../../types/types";
 
 export async function getServerSideProps(context: any) {
   const myHeaders = new Headers();
@@ -18,23 +19,21 @@ export async function getServerSideProps(context: any) {
 
     return { props: movie };
   } catch (error) {
-    const movie = { problem: "Not Found" };
+    const movie = { error: "Not Found" };
     console.error(`Error fetching movie data for "${slug}": ${error}`);
     return { props: movie };
   }
 }
 
-export default function Home(movie: any) {
+export default function Home(movie: movie) {
   const [movieInformation, setMovieInformation] = useState(movie);
-
-  // console.log(movie);
 
   return (
     <>
       <Navbar />
 
       <main className=" w-screen  bg-neutral-200 ">
-        {movie.problem == undefined ? (
+        {movie.error == undefined ? (
           <MovieDescription movie={movie} />
         ) : (
           <h2 className="mx-auto mt-[10rem] px-8 text-3xl leading-relaxed">
